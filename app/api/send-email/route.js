e89f32342181f2124cdfc0ac4dbebbba0c111543
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2024-10-04 16:00:18 +0500

    c

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -13,8 +13,8 @@ export async function POST(request) {
     try {
         transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // Use TLS
+            port: 465,
+            secure: true, // Use TLS
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
